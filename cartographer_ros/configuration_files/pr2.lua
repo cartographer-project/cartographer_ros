e1aaf65ac399dece7cbf3c5bacefaafe4854348c
Damon Kohler <damonkohler@gmail.com>
2016-09-30 15:09:31 +0200

    Adds PR2 demo and script for dropping TF frames. (#77)

---
+++ cartographer_ros/configuration_files/pr2.lua
@@ -0,0 +1,46 @@
+-- Copyright 2016 The Cartographer Authors
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+include "map_builder.lua"
+
+options = {
+  map_builder = MAP_BUILDER,
+  map_frame = "map",
+  tracking_frame = "base_footprint",
+  published_frame = "base_footprint",
+  odom_frame = "odom",
+  provide_odom_frame = true,
+  use_odometry_data = false,
+  use_constant_odometry_variance = false,
+  constant_odometry_translational_variance = 0.,
+  constant_odometry_rotational_variance = 0.,
+  use_horizontal_laser = true,
+  use_horizontal_multi_echo_laser = false,
+  horizontal_laser_min_range = 0.,
+  horizontal_laser_max_range = 30.,
+  horizontal_laser_missing_echo_ray_length = 5.,
+  num_lasers_3d = 0,
+  lookup_transform_timeout_sec = 0.2,
+  submap_publish_period_sec = 0.3,
+  pose_publish_period_sec = 5e-3,
+}
+
+MAP_BUILDER.use_trajectory_builder_2d = true
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.15
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(35.)
+SPARSE_POSE_GRAPH.optimization_problem.huber_scale = 1e2
+
+return options
---
+++ cartographer_ros/scripts/tf_remove_frames.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright 2016 The Cartographer Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rospy
+from tf.msg import tfMessage
+
+
+def main():
+  rospy.init_node('tf_remove_frames')
+  publisher = rospy.Publisher('/tf_out', tfMessage, queue_size=1)
+  remove_frames = rospy.get_param('~remove_frames', [])
+
+  def callback(msg):
+    msg.transforms = [t for t in msg.transforms if
+                      t.header.frame_id.lstrip('/') not in remove_frames and
+                      t.child_frame_id.lstrip('/') not in remove_frames]
+    publisher.publish(msg)
+
+  rospy.Subscriber('/tf_in', tfMessage, callback)
+  rospy.spin()
+
+
+if __name__ == '__main__':
+  main()
