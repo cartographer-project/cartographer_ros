0eedef4ed396d0cc739ba2fab939e25f93a67a19
Damon Kohler <damonkohler@google.com>
2016-09-30 14:24:19 +0200

    Addressess comments.

--- cartographer_ros/scripts/tf_remove_frames.py
+++ cartographer_ros/scripts/tf_remove_frames.py
@@ -19,17 +19,17 @@ import rospy
 from tf.msg import tfMessage
 
 
-def callback(msg):
-  msg.transforms = [t for t in msg.transforms if
-                    t.header.frame_id.lstrip('/') not in remove_frames and
-                    t.child_frame_id.lstrip('/') not in remove_frames]
-  publisher.publish(msg)
-
-
 def main():
   rospy.init_node('tf_remove_frames')
   publisher = rospy.Publisher('/tf_out', tfMessage, queue_size=1)
   remove_frames = rospy.get_param('~remove_frames', [])
+
+  def callback(msg):
+    msg.transforms = [t for t in msg.transforms if
+                      t.header.frame_id.lstrip('/') not in remove_frames and
+                      t.child_frame_id.lstrip('/') not in remove_frames]
+    publisher.publish(msg)
+
   rospy.Subscriber('/tf_in', tfMessage, callback)
   rospy.spin()
 
