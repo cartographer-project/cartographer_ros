3a413028d95ea548a2b4b1807282a36826846755
Damon Kohler <damonkohler@gmail.com>
2016-09-30 14:25:28 +0200

    Adds Revo LDS demo. (#76)
    * Adds Revo LDS demo.

---
+++ cartographer_ros/configuration_files/revo_lds.lua
@@ -0,0 +1,44 @@
+-- Copyright 2016 The Cartographer Authors
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+include "map_builder.lua"
+
+options = {
+  map_builder = MAP_BUILDER,
+  map_frame = "map",
+  tracking_frame = "horizontal_laser_link",
+  published_frame = "horizontal_laser_link",
+  odom_frame = "odom",
+  provide_odom_frame = true,
+  use_odometry_data = false,
+  use_constant_odometry_variance = false,
+  constant_odometry_translational_variance = 0.,
+  constant_odometry_rotational_variance = 0.,
+  use_horizontal_laser = true,
+  use_horizontal_multi_echo_laser = false,
+  horizontal_laser_min_range = 0.3,
+  horizontal_laser_max_range = 8.,
+  horizontal_laser_missing_echo_ray_length = 1.,
+  num_lasers_3d = 0,
+  lookup_transform_timeout_sec = 0.01,
+  submap_publish_period_sec = 0.3,
+  pose_publish_period_sec = 5e-3,
+}
+
+MAP_BUILDER.use_trajectory_builder_2d = true
+TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+SPARSE_POSE_GRAPH.optimization_problem.huber_scale = 1e2
+
+return options
