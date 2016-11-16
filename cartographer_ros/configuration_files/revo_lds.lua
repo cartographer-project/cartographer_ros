f6230c009abde2a15cb8fa64e4fc9c604dbe2257
Damon Kohler <damonkohler@google.com>
2016-11-16 16:22:03 +0100

    Adds configuration files test.

--- cartographer_ros/configuration_files/revo_lds.lua
+++ cartographer_ros/configuration_files/revo_lds.lua
@@ -36,9 +36,9 @@ options = {
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 
-TRAJECTORY_BUILDER_2D.laser_min_range = 0.3,
-TRAJECTORY_BUILDER_2D.laser_max_range = 8.,
-TRAJECTORY_BUILDER_2D.laser_missing_echo_ray_length = 1.,
+TRAJECTORY_BUILDER_2D.laser_min_range = 0.3
+TRAJECTORY_BUILDER_2D.laser_max_range = 8.
+TRAJECTORY_BUILDER_2D.laser_missing_echo_ray_length = 1.
 TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 
