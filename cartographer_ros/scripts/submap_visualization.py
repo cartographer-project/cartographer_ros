06010bd22081a560ae50c589d817eaa0c1c5e5ff
Jihoon Lee <lee@magazino.eu>
2017-03-31 17:32:26 +0200

    fix interactivity

--- cartographer_ros/scripts/submap_visualization.py
+++ cartographer_ros/scripts/submap_visualization.py
@@ -8,52 +8,46 @@ import gzip
 import StringIO
 from time import sleep
 
-global last_map_id, n_submaps
 
 def submap_list_callback(submap_list):
-    n_submaps = submap_list.trajectory[0].submap.__len__()-1
+    n_submaps = len(submap_list.trajectory[0].submap) - 1
     cv2.setTrackbarMax('submap_id', 'img', n_submaps)
-    #cv2.updateWindow('img')
+
+
+def get_img(submap):
+    fileobject = StringIO.StringIO(submap.cells)
+    f = gzip.GzipFile(fileobj=fileobject)
+    string = f.read()
+    img = np.fromstring(string, np.uint8)
+    img = img.reshape((submap.height, submap.width, -1))
+    img = np.concatenate((img[:, :, 1:], img[:, :, 1:], img[:, :, :1]), axis=2)
+    return img
+
+
+def fetch_and_render(map_id):
+    submap = submap_query(0, map_id)
+    img = get_img(submap)
+    cv2.imshow('img',img)
+
+
+def callback(mapid):
+    fetch_and_render(mapid)
 
 
 if __name__ == "__main__":
+    cv2.namedWindow('img', flags=cv2.WINDOW_NORMAL)
+    trackbar = cv2.createTrackbar('submap_id', 'img', 0, 1, callback)
+
     rospy.init_node("submap_visualization")
-    rospy.Subscriber("/submap_list",SubmapList, submap_list_callback)
 
     submap_query = rospy.ServiceProxy('submap_query', SubmapQuery)
-
-    last_map_id = 0
-
-    def callback(mapid):
-        global last_map_id
-        last_map_id = mapid
-
-
-    def fetch_and_render():
-        submap = submap_query(0, last_map_id)
-        fileobject = StringIO.StringIO(submap.cells)
-        f = gzip.GzipFile(fileobj=fileobject)
-        string = f.read()
-        img = np.fromstring(string, np.uint8)
-
-        print img.shape, submap.width, submap.height, submap.resolution
-        img = img.reshape((submap.height, submap.width, -1))
-
-        img = np.concatenate((img[:, :, :1], img[:, :, 1:], img[:, :, 1:]), axis=2)
-        cv2.setWindowProperty("img", cv2.WINDOW_AUTOSIZE, False)
-        cv2.imshow("img", img)
-        #cv2.resizeWindow("img", 500, 500)
-
-
-    cv2.namedWindow('img')
-    trackbar = cv2.createTrackbar('submap_id', 'img', 0, 50, callback)
-
+    rospy.loginfo("Wait for Submap Query")
     rospy.wait_for_service('/submap_query')
+    rospy.loginfo("Submap Query Ready")
+    rospy.loginfo("Initialized")
+    rospy.Subscriber("/submap_list",SubmapList, submap_list_callback)
 
-    print type(trackbar)
-    while not cv2.waitKey(1) & 0xFF == 27:
-        try:
-            fetch_and_render()
-        except Exception,e:
-            print e
-        sleep(0.05)
+    while not rospy.is_shutdown():
+        k = cv2.waitKey(1)
+        if k==27:    # Esc key to stop
+            break
