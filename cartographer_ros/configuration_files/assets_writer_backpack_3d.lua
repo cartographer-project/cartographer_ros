cd1276a99ccffcc491d0b2e50296ec04347ba5f2
Wolfgang Hess <whess@google.com>
2017-05-24 15:27:52 +0200

    Add an assets pipeline demo for 3D. (#352)

--- cartographer_ros/configuration_files/assets_writer_backpack_3d.lua
+++ cartographer_ros/configuration_files/assets_writer_backpack_3d.lua
@@ -12,11 +12,6 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
--- WARNING: we create a lot of X-Rays of a potentially large space in this
--- pipeline. For example, running over the
--- cartographer_paper_deutsches_museum.bag requires ~25GiB of memory. You can
--- reduce this by writing fewer X-Rays or upping VOXEL_SIZE - which is the size
--- of a pixel in a X-Ray.
 VOXEL_SIZE = 5e-2
 
 XY_TRANSFORM =  {
@@ -66,57 +61,17 @@ options = {
       transform = XZ_TRANSFORM,
     },
 
-    -- We now use the intensities to color our points. We apply a linear
-    -- transform to clamp our intensity values into [0, 255] and then use this
-    -- value for RGB of our points. Every stage in the pipeline after this now
-    -- receives colored points.
-    --
-    -- We write xrays again. These now use geometry and the intensities to
-    -- color pixels - they look quite similar, just a little lighter.
-    {
-      action = "intensity_to_color",
-      min_intensity = 0.,
-      max_intensity = 4095.,
-    },
-
-    {
-      action = "write_xray_image",
-      voxel_size = VOXEL_SIZE,
-      filename = "xray_yz_all_intensity",
-      transform = YZ_TRANSFORM,
-    },
-    {
-      action = "write_xray_image",
-      voxel_size = VOXEL_SIZE,
-      filename = "xray_xy_all_intensity",
-      transform = XY_TRANSFORM,
-    },
-    {
-      action = "write_xray_image",
-      voxel_size = VOXEL_SIZE,
-      filename = "xray_xz_all_intensity",
-      transform = XZ_TRANSFORM,
-    },
-
-    -- We also write a PLY file at this stage, because gray points look good.
-    -- The points in the PLY can be visualized using
-    -- https://github.com/googlecartographer/point_cloud_viewer.
-    {
-      action = "write_ply",
-      filename = "points.ply",
-    },
-
     -- Now we recolor our points by frame and write another batch of X-Rays. It
     -- is visible in them what was seen by the horizontal and the vertical
     -- laser.
     {
       action = "color_points",
-      frame_id = "horizontal_laser_link",
+      frame_id = "horizontal_vlp16_link",
       color = { 255., 0., 0. },
     },
     {
       action = "color_points",
-      frame_id = "vertical_laser_link",
+      frame_id = "vertical_vlp16_link",
       color = { 0., 255., 0. },
     },
 
