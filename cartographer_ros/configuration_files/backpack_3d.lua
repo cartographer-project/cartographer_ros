bf16ec645829ce2299903133b310c5d1644348e8
Wolfgang Hess <whess@google.com>
2017-06-16 17:45:11 +0200

    Simplify the configuration files. (#383)

--- cartographer_ros/configuration_files/backpack_3d.lua
+++ cartographer_ros/configuration_files/backpack_3d.lua
@@ -37,13 +37,13 @@ TRAJECTORY_BUILDER_3D.scans_per_accumulation = 160
 
 MAP_BUILDER.use_trajectory_builder_3d = true
 MAP_BUILDER.num_background_threads = 7
-MAP_BUILDER.sparse_pose_graph.optimization_problem.huber_scale = 5e2
-MAP_BUILDER.sparse_pose_graph.optimize_every_n_scans = 320
-MAP_BUILDER.sparse_pose_graph.constraint_builder.sampling_ratio = 0.03
-MAP_BUILDER.sparse_pose_graph.optimization_problem.ceres_solver_options.max_num_iterations = 10
+SPARSE_POSE_GRAPH.optimization_problem.huber_scale = 5e2
+SPARSE_POSE_GRAPH.optimize_every_n_scans = 320
+SPARSE_POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
+SPARSE_POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
 -- Reuse the coarser 3D voxel filter to speed up the computation of loop closure
 -- constraints.
-MAP_BUILDER.sparse_pose_graph.constraint_builder.adaptive_voxel_filter = TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter
-MAP_BUILDER.sparse_pose_graph.constraint_builder.min_score = 0.62
+SPARSE_POSE_GRAPH.constraint_builder.adaptive_voxel_filter = TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter
+SPARSE_POSE_GRAPH.constraint_builder.min_score = 0.62
 
 return options
