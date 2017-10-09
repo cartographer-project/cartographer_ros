4e9ad6b5b76fd642ced4d857fe4f6cf378afe30b
Holger Rapp <hrapp@google.com>
2017-10-09 13:22:56 +0200

    More linting.

--- cartographer_ros/scripts/remove_leading_slashes.py
+++ cartographer_ros/scripts/remove_leading_slashes.py
@@ -14,9 +14,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """A simple tool to remove leading slashes from frame names."""
-
 
 import argparse
 import os
