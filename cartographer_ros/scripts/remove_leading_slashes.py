2fbca4a2df70526fe855556e7e29fdbc5a0e355b
Holger Rapp <hrapp@google.com>
2017-10-09 12:10:52 +0200

    Addressed reviewer comments.

--- cartographer_ros/scripts/remove_leading_slashes.py
+++ cartographer_ros/scripts/remove_leading_slashes.py
@@ -1,19 +1,36 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-"""A simple tool to remove leading slashes from bags."""
+# Copyright 2016 The Cartographer Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-import rosbag
+"""A simple tool to remove leading slashes from frame names."""
+
+
 import argparse
+import os
+import rosbag
 
-def parse_args():
-  p = argparse.ArgumentParser(description=
-    "Removes leading slashes from topic names in a bag."
-  )
-  p.add_argument("input", type=str, help = "Input bag")
+
+def ParseArgs():
+  p = argparse.ArgumentParser(
+      description="Removes leading slashes from frame names.")
+  p.add_argument("input", type=str, help="Input bag")
   return p.parse_args()
 
-def rewrite(msg):
+
+def RewriteMsg(msg):
   if hasattr(msg, "header"):
     if msg.header.frame_id.startswith("/"):
       msg.header.frame_id = msg.header.frame_id[1:]
@@ -22,14 +39,17 @@ def rewrite(msg):
       msg.child_frame_id = msg.child_frame_id[1:]
   if hasattr(msg, "transforms"):
     for transform_msg in msg.transforms:
-      rewrite(transform_msg)
+      RewriteMsg(transform_msg)
 
-def main():
-  o = parse_args()
-  with rosbag.Bag(o.input[:-4] + '.filtered.bag', 'w') as outbag:
-      for topic, msg, t in rosbag.Bag(o.input).read_messages():
-        rewrite(msg)
-        outbag.write(topic, msg, msg.header.stamp if msg._has_header else t)
+
+def Main():
+  options = ParseArgs()
+  with rosbag.Bag(os.path.splitext(options.input)[0] + ".filtered.bag",
+                  "w") as outbag:
+    for topic, msg, t in rosbag.Bag(options.input).read_messages():
+      RewriteMsg(msg)
+      outbag.write(topic, msg, msg.header.stamp if msg._has_header else t)
+
 
 if __name__ == "__main__":
-  main()
+  Main()
