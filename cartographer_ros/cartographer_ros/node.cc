e18605e918ae61ea2daae5474ddf38d4a8d8cdc9
Holger Rapp <hrapp@google.com>
2017-11-14 11:12:56 +0100

    Ran clang-format.

--- cartographer_ros/cartographer_ros/node.cc
+++ cartographer_ros/cartographer_ros/node.cc
@@ -381,7 +381,8 @@ bool Node::FinishTrajectoryUnderLock(const int trajectory_id) {
     return false;
   }
   if (!is_active_trajectory_[trajectory_id]) {
-    LOG(INFO) << "Trajectory_id " << trajectory_id << " has already been finished.";
+    LOG(INFO) << "Trajectory_id " << trajectory_id
+              << " has already been finished.";
     return false;
   }
 
