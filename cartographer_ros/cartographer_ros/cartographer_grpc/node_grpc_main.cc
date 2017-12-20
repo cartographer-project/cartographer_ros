abe4d10de355f75df65ca935c47f0e50c925c8c1
Christoph Schütte <cschuet@google.com>
2017-12-20 12:32:40 +0100

    Add BUILD_GRPC CMake flag and ROS-gRPC binary. (#631)

---
+++ cartographer_ros/cartographer_ros/cartographer_grpc/node_grpc_main.cc
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2017 The Cartographer Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "cartographer_ros/node.h"
+#include "cartographer_ros/node_options.h"
+#include "cartographer_ros/ros_log_sink.h"
+#include "gflags/gflags.h"
+
+DEFINE_string(configuration_directory, "",
+              "First directory in which configuration files are searched, "
+              "second is always the Cartographer installation to allow "
+              "including files from there.");
+DEFINE_string(configuration_basename, "",
+              "Basename, i.e. not containing any directory prefix, of the "
+              "configuration file.");
+DEFINE_string(map_filename, "", "If non-empty, filename of a map to load.");
+DEFINE_bool(
+    start_trajectory_with_default_topics, true,
+    "Enable to immediately start the first trajectory with default topics.");
+DEFINE_string(
+    save_map_filename, "",
+    "If non-empty, serialize state and write it to disk before shutting down.");
+
+namespace cartographer_ros {
+namespace cartographer_grpc {
+namespace {
+
+void Run() {
+  // TODO(cschuet): Implement this.
+}
+
+}  // namespace
+}  // namespace cartographer_grpc
+}  // namespace cartographer_ros
+
+int main(int argc, char** argv) {
+  google::InitGoogleLogging(argv[0]);
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  CHECK(!FLAGS_configuration_directory.empty())
+      << "-configuration_directory is missing.";
+  CHECK(!FLAGS_configuration_basename.empty())
+      << "-configuration_basename is missing.";
+
+  ::ros::init(argc, argv, "cartographer_grpc_node");
+  ::ros::start();
+
+  cartographer_ros::ScopedRosLogSink ros_log_sink;
+  cartographer_ros::cartographer_grpc::Run();
+  ::ros::shutdown();
+}
