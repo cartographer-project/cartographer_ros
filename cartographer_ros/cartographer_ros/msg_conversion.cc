92b1ce16a485b45eb10b37c3a0e70f643f923589
pifon <pifon@google.com>
2018-02-07 15:09:17 +0100

    Ran clang-format.

--- cartographer_ros/cartographer_ros/msg_conversion.cc
+++ cartographer_ros/cartographer_ros/msg_conversion.cc
@@ -322,8 +322,10 @@ std::unique_ptr<nav_msgs::OccupancyGrid> CreateOccupancyGridMsg(
       const uint32_t packed = pixel_data[y * width + x];
       const unsigned char color = packed >> 16;
       const unsigned char observed = packed >> 8;
-      const int value = observed == 0 ? -1 : ::cartographer::common::RoundToInt(
-                                                 (1. - color / 255.) * 100.);
+      const int value =
+          observed == 0
+              ? -1
+              : ::cartographer::common::RoundToInt((1. - color / 255.) * 100.);
       CHECK_LE(-1, value);
       CHECK_GE(100, value);
       occupancy_grid->data.push_back(value);
