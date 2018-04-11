ee95063c5c005fca5f1ebe0b783b2a0a893358bd
Andre Gaschler <gaschler@google.com>
2018-04-11 11:24:10 +0200

    minimal rostest works with --catkin-make-args test, but not run_tests

--- cartographer_ros/cartographer_ros/result_subscriber_test.cc
+++ cartographer_ros/cartographer_ros/result_subscriber_test.cc
@@ -28,48 +28,9 @@
 namespace cartographer_ros {
 namespace {
 
-TEST(ResultSubscriberTest, ReceiveMovingBaseLink) {
-  ::ros::init(std::map<std::string, std::string>{}, "result_subscriber_test");
-  ScopedRosLogSink ros_log_sink;
-  ros::start();
-  ::ros::NodeHandle node_handle;
-  ::tf2_ros::Buffer buffer(ros::Duration(60));
-  ::tf2_ros::TransformListener listener(buffer);
-  std::vector<::cartographer::transform::Rigid3d> result_transforms;
-  std::vector<geometry_msgs::TransformStamped> result_messages;
-  const ros::Duration kSimulationRunTime(5);
-  const ros::WallDuration kMaximumWaitTime(20);
-  ::ros::Rate rate(100);
-  ::ros::WallTime wall_time_start = ros::WallTime::now();
-  ::ros::Duration observed_duration(0);
-  while (observed_duration < kSimulationRunTime &&
-         ros::WallTime::now() < wall_time_start + kMaximumWaitTime) {
-    rate.sleep();
-    try {
-      auto transform = buffer.lookupTransform("map", "base_link", ros::Time(0));
-      result_transforms.push_back(ToRigid3d(transform));
-      result_messages.push_back(transform);
-      observed_duration = result_messages.back().header.stamp -
-                          result_messages.front().header.stamp;
-    } catch (tf2::TransformException& e) {
-      LOG(INFO) << e.what();
-    }
-  }
-  EXPECT_GE(result_messages.size(), 10)
-      << "Did not receive enough map to base_link transforms.";
-  EXPECT_NEAR(observed_duration.toSec(), kSimulationRunTime.toSec(), 2.0);
-  ASSERT_GT(result_transforms.size(), 0);
-  double travel_distance = (result_transforms.back().translation() -
-                            result_transforms.front().translation())
-                               .norm();
-  LOG(INFO) << "Observed simulation time:" << observed_duration;
-  LOG(INFO) << "Observed travel distance:" << travel_distance;
-  // TODO(gaschler): gtest cannot read arguments, so the expected range needs to
-  // be large.
-  const double kMinimumTravelDistance = 0.2;
-  const double kMaximumTravelDistance = 5.0;
-  EXPECT_GE(travel_distance, kMinimumTravelDistance);
-  EXPECT_LE(travel_distance, kMaximumTravelDistance);
+TEST(ResultSubscriberTest, MinimalTest) {
+  std::cout << "MinimalTest1";
+  EXPECT_TRUE(true);
 }
 
 }  // namespace
