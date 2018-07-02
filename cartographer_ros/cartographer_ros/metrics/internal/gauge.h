237dd83b604bd6bb2039e994ed34d981de5e82f0
Michael Grupp <grupp@magazino.eu>
2018-07-02 10:26:58 +0200

    Add internal metric families. (#914)
    - minimal counter, gauge and histogram implementations
    - metric family interfaces as defined in libcartographer
    - serializable to ROS messages
    
    RFC: https://github.com/googlecartographer/rfcs/pull/26

---
+++ cartographer_ros/cartographer_ros/metrics/internal/gauge.h
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2018 The Cartographer Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CARTOGRAPHER_ROS_METRICS_INTERNAL_GAUGE_H
+#define CARTOGRAPHER_ROS_METRICS_INTERNAL_GAUGE_H
+
+#include <atomic>
+#include <map>
+#include <string>
+
+#include "cartographer/metrics/gauge.h"
+#include "cartographer_ros_msgs/Metric.h"
+
+namespace cartographer_ros {
+namespace metrics {
+
+class Gauge : public ::cartographer::metrics::Gauge {
+ public:
+  explicit Gauge(const std::map<std::string, std::string>& labels)
+      : labels_(labels) {}
+
+  void Decrement(const double by_value) override { Add(-1. * by_value); }
+
+  void Decrement() override { Decrement(1.); }
+
+  void Increment(const double by_value) override { Add(by_value); }
+
+  void Increment() override { Increment(1.); }
+
+  void Set(double value) override {
+    double expected = value_.load();
+    while (!value_.compare_exchange_weak(expected, value)) {
+      ;
+    }
+  }
+  double Value() const { return value_.load(); }
+
+  cartographer_ros_msgs::Metric ToRosMessage() {
+    cartographer_ros_msgs::Metric msg;
+    msg.type = cartographer_ros_msgs::Metric::TYPE_GAUGE;
+    for (const auto& label : labels_) {
+      cartographer_ros_msgs::MetricLabel label_msg;
+      label_msg.key = label.first;
+      label_msg.value = label.second;
+      msg.labels.push_back(label_msg);
+    }
+    msg.value = Value();
+    return msg;
+  }
+
+ private:
+  void Add(const double value) {
+    double current = Value();
+    Set(current + value);
+  }
+
+  const std::map<std::string, std::string> labels_;
+  std::atomic<double> value_{0.};
+};
+
+}  // namespace metrics
+}  // namespace cartographer_ros
+
+#endif  // CARTOGRAPHER_ROS_METRICS_INTERNAL_GAUGE_H
---
+++ cartographer_ros/cartographer_ros/metrics/internal/histogram.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2018 The Cartographer Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CARTOGRAPHER_ROS_METRICS_INTERNAL_HISTOGRAM_H
+#define CARTOGRAPHER_ROS_METRICS_INTERNAL_HISTOGRAM_H
+
+#include <map>
+#include <vector>
+
+#include "cartographer/common/mutex.h"
+#include "cartographer/metrics/histogram.h"
+#include "cartographer_ros_msgs/Metric.h"
+
+namespace cartographer_ros {
+namespace metrics {
+
+constexpr double kInfiniteBoundary = std::numeric_limits<double>::infinity();
+
+using BucketBoundaries = ::cartographer::metrics::Histogram::BucketBoundaries;
+
+class Histogram : public ::cartographer::metrics::Histogram {
+ public:
+  explicit Histogram(const std::map<std::string, std::string>& labels,
+                     const BucketBoundaries& bucket_boundaries);
+
+  explicit Histogram(const BucketBoundaries& bucket_boundaries);
+
+  void Observe(double value) override;
+
+  std::map<double, double> CountsByBucket();
+
+  double Sum();
+
+  double CumulativeCount();
+
+  cartographer_ros_msgs::Metric ToRosMessage();
+
+ private:
+  cartographer::common::Mutex mutex_;
+  const std::map<std::string, std::string> labels_;
+  const BucketBoundaries bucket_boundaries_;
+  std::vector<double> bucket_counts_ GUARDED_BY(mutex_);
+  double sum_ GUARDED_BY(mutex_);
+};
+
+}  // namespace metrics
+}  // namespace cartographer_ros
+
+#endif  // CARTOGRAPHER_ROS_METRICS_INTERNAL_HISTOGRAM_H
