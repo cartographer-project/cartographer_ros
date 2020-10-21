dcbd9d10dfcd46dcbf883410a6b2ae397a8e2de7
Jonathan Huber <jonathan.huber@enway.ai>
2020-10-21 14:03:15 +0200

    Make publishing tf optional, enable publishing PoseStamped (#1099)
    If the output of cartographer should be used as an input to an additional sensor fusion method,
    using the published TF transform is inconvenient or in our specific use case even harmful
    because we don't want to add the raw cartographer output to our TF tree.
    
    With this change it becomes optional to broadcast to /tf.
    Morever there is an option to publish the tracked frame pose as a PoseStamped message.
    We add two new optional parameters:
    - `publish_to_tf` if false no tf transform is broadcasted
    -  `publish_tracked_pose` If set `true` a PoseStamped representing the position of the
    tracked pose w.r.t. map frame is published.
    
    If default launchers and settings are used this PR causes no change in the behavior.

--- cartographer_ros/cartographer_ros/node.h
+++ cartographer_ros/cartographer_ros/node.h
@@ -188,6 +188,7 @@ class Node {
   ::ros::Publisher trajectory_node_list_publisher_;
   ::ros::Publisher landmark_poses_list_publisher_;
   ::ros::Publisher constraint_list_publisher_;
+  ::ros::Publisher tracked_pose_publisher_;
   // These ros::ServiceServers need to live for the lifetime of the node.
   std::vector<::ros::ServiceServer> service_servers_;
   ::ros::Publisher scan_matched_point_cloud_publisher_;
