1c9274ffeeb0253f0894b8d89f4fae5121ac165e
Michael Grupp <grupp@magazino.eu>
2020-10-22 17:33:45 +0200

    Default to master in rosinstall (fixes #1122). (#1530)
    This makes much more sense than 1.0.0, which is counter-intuitive.
    
    If someone is building from source they usually do it to build the
    latest bleeding-edge version.
    
    Signed-off-by: Michael Grupp <grupp@magazino.eu>

--- scripts/prepare_catkin_workspace.sh
+++ scripts/prepare_catkin_workspace.sh
@@ -26,6 +26,10 @@ wstool init
 
 # Merge the cartographer_ros.rosinstall file and fetch code for dependencies.
 wstool merge ../../cartographer_ros/cartographer_ros.rosinstall
-wstool set cartographer -v ${CARTOGRAPHER_VERSION} -y
+# We default to master and wstool seems to have a bug when it's being set twice.
+# TODO(MichaelGrupp): wstool is unmaintained, use vcstool.
+if [ ${CARTOGRAPHER_VERSION} != "master" ]; then
+  wstool set cartographer -v ${CARTOGRAPHER_VERSION} -y
+fi
 wstool remove cartographer_ros
 wstool update
