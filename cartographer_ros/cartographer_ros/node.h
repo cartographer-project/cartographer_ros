0b66821840819846f4250f3ad2b16228edd893af
stribor14 <mirkokot@gmail.com>
2020-11-13 13:22:31 +0100

    Check if we already publihed transformation at same timestamp Fix #1555 (#1556)
    Signed-off-by: stribor14 <mirko.kokot@romb-technologies.hr>
    
    Co-authored-by: stribor14 <mirko.kokot@romb-technologies.hr>

--- cartographer_ros/cartographer_ros/node.h
+++ cartographer_ros/cartographer_ros/node.h
@@ -214,6 +214,7 @@ class Node {
 
   // These are keyed with 'trajectory_id'.
   std::map<int, ::cartographer::mapping::PoseExtrapolator> extrapolators_;
+  std::map<int, ::ros::Time> last_published_tf_stamps_;
   std::unordered_map<int, TrajectorySensorSamplers> sensor_samplers_;
   std::unordered_map<int, std::vector<Subscriber>> subscribers_;
   std::unordered_set<std::string> subscribed_topics_;
